68a51661a40f8168e0aed1671ba67c3424d46594
Ahamed Basith <ahamedb254@gmail.com>
2024-09-18 20:19:03 +0530

    Add files via upload

---
+++ T1.java
@@ -0,0 +1,29 @@
+package printhello;
+
+public class T1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Common Datatypes and Variables name and Value.
+		int a = 20;
+		int b = 10;
+		int c = 40;
+		
+		//Multiply the value
+		int d = (a*b*c);
+        System.out.println("The Multiply total value (a,b,c):"+d);
+        
+        //Adding the value
+        int e = (a+b+c);
+        System.out.println("The adding total value (a,b,c):"+e);
+        
+        //Subtract the value
+        int f = (c-b-a);
+        System.out.println("The Subtract total value (c,b,a):"+f);
+        
+        //Divide the value
+        int g = (c/a);
+        System.out.println("The Divide total value (c/a):"+g);
+	}
+
+}
---
+++ T2.java
@@ -0,0 +1,25 @@
+package printhello;
+
+import java.util.*;
+
+public class T2 {
+    static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the String Name");
+		String name=sc.nextLine();
+		
+		System.out.println("Enter the Integer Number");
+	    int num=sc.nextInt();
+	    
+	    System.out.println("Enter the Double Value");
+	    double val=sc.nextDouble();
+	    
+		System.out.println("The String Name is:"+name);
+		System.out.println("The Integer number is:"+num);
+		System.out.println("The Double value is:"+val);
+       
+	}
+	
+}
+

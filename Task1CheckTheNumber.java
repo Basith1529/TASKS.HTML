2bed5e39976d05af1f3ddc6ac7da1e6b72cc1423
Ahamed Basith <ahamedb254@gmail.com>
2024-09-18 20:21:10 +0530

    Add files via upload

---
+++ Task1CheckTheNumber.java
@@ -0,0 +1,48 @@
+package program;
+
+import java.util.*;
+
+public class Task1CheckTheNumber {
+    static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number");
+		
+		/*int a=0;
+		
+		
+		if(a>0) {
+			
+			System.out.println("Positive");
+		}
+		
+		else if(a<0) {
+			
+			System.out.println("Negative");
+		}
+		
+		else {
+			
+			System.out.println("Zero");
+		}*/
+		
+		int b = sc.nextInt();
+		
+         if(b>0) {
+			
+			System.out.println("Positive");
+		}
+		
+		else if(b<0) {
+			
+			System.out.println("Negative");
+		}
+		
+		else {
+			
+			System.out.println("Zero");
+		}
+		
+	}
+
+}
---
+++ Task2PrintEmployeeIfElseIf.java
@@ -0,0 +1,32 @@
+package program;
+
+public class Task2PrintEmployeeIfElseIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 100;
+		
+		if(age>=90) {
+			
+			System.out.println("You are not eligible to work in the company");
+		}
+		
+//		else if(age>=40 && age<=50) {
+			
+//			System.out.println("You are eligible rare to work in the company");
+//		}
+		
+//		else if(age>=50 && age<=60) {
+			
+//			System.out.println("You are Outside the company");
+//		}
+		
+		else {
+			
+			System.out.println("You are eligible to work in the company");
+		}
+
+	}
+
+}
